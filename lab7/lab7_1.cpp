61e0be148336dc1d64d2f07c983fdf84fb501193
drewxa <a.borodin@bmstu.ru>
2017-11-09 23:17:51 +0300

    added comments

--- lab7/lab7_1.cpp
+++ lab7/lab7_1.cpp
@@ -5,9 +5,14 @@
 
 std::string readFromFile(const std::string& filename)
 {
+    // открываем файл с именем filename
+    // std::ifstream работает по аналогии с std::cin, только данные считываются не из консоли, а из файла
+    // std::ofstream работает по аналогии с std::cout, только данные выводятся не в консоль, а записываются в файл
+    // std::fstream объединяет в себе std::ifstream и std::ofstream, и работает в зависимости от второго аргумента
     std::fstream fl(filename);
 
     std::string str;
+    // используем оператор >> для получения данных из файла
     fl >> str;
 
     return str;
@@ -15,13 +20,20 @@ std::string readFromFile(const std::string& filename)
 
 void writeToFile(const std::string& filename, const std::string& data)
 {
+    // открываем файл с именем filename
+    // второй аргумент означает, что файл для записи в него данных
+    // старые данные, которые находятся в файле удалятся
     std::fstream fl(filename, std::ios::out);
 
+    // используем оператор << для записи данных в файл
     fl << data;
 }
 
 void appendToFile(const std::string& filename, const std::string& data)
 {
+    // второй аргумент означает, что файл для записи в него данных
+    // старые данные, которые находятся в файле останутся
+    // новые данные будут добавляться в конец файла
     std::fstream fl(filename, std::ios::app);
 
     fl << data;
@@ -29,6 +41,10 @@ void appendToFile(const std::string& filename, const std::string& data)
 
 void vectorToFile(const std::string& filename, const std::vector<int>& data)
 {
+    // открывваем файл с именем filename
+    // файл открывается только для записи в него данных
+    // считывать данные из файла используя std::ofstream нельзя
+    // когда файл открывается для записи данных, другие программы не могут читать или записывать данные в него
     std::ofstream fl(filename);
 
     for (int i = 0; i < data.size(); ++i)
@@ -40,11 +56,16 @@ void vectorToFile(const std::string& filename, const std::vector<int>& data)
     }
 }
 
-
 void vectorFromFile(const std::string& filename, std::vector<int>& data)
 {
+    // открывваем файл с именем filename
+    // файл открывается только для чтения данных из файла
+    // записывать данные в файл нельзя, используя std::ifstream, нельзя
+    // зато можно читать этот файл из нескольких программ
     std::ifstream fl(filename);
 
+    // метод fl.eof() возвращает true, если все данные из файла были считаны
+    // т.е.конструкция while (!fl.eof()) означает: "читаем файл до тех пор пока есть что читать"
     while (!fl.eof())
     {
         int e = 0;
